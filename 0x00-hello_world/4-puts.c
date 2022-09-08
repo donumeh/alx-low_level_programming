8bd413d1e1f33e7728f79b3368d3e432736db452
Donald Umeh <donumeh54@gmail.com>
2022-09-08 14:24:14 +0100

    printed string with puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints a string
+ *
+ * Return: Always Zero (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	
+	return (0);
+}
