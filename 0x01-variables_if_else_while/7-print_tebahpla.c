175ae8c40cf33bc8feb25dcb1a5d3ce3c5442a9c
Donald Umeh <donumeh54@gmai.com>
2022-09-09 08:26:56 -0700

    print alpha in lowercase backwards

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints the lowercase alpha backwards
+ * 
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alpha_small;
+
+	for (alpha_small = 'z'; alpha_small >= 'a'; alpha_small--)
+		putchar(alpha_small);
+	putchar('\n');
+
+	return (0);
+}
