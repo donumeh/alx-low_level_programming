250fe3b33c2bf2ba136bcf1bb00588b3a4ca8840
Donald Umeh <donumeh54@gmail.com>
2022-09-09 23:51:25 +0100

    combination

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - print possible combinations of two two-dgits
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, j, k, l;
+
+	for (i = 48; i <= 57; i++)
+		for (j = 48; j <= 56; j++)
+			for (k = 48; k <= 57; k++)
+				for (l = 49; l <= 57; l++)
+				{
+					if (i > k && j >= l)
+						continue;
+					putchar(i);
+					putchar(j);
+					putchar(' ');
+					putchar(k);
+					putchar(l);
+					if (!(i == 57 && j == 56 && k == 57 && l == 57))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+	putchar('\n');
+
+	return (0);
+}
