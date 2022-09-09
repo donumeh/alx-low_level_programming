e50c02cea8418aa187664683ddb2e0e5ef1b0fba
Donald Umeh <donumeh54@gmai.com>
2022-09-09 07:55:57 -0700

    prints alpha in lowercase exempt q & e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints the lowercase alpha except 'q' and 'e'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alpha_small = 'a';
+
+	while (alpha_small <= 'z');
+	{
+		if (alpha_small == 'q' || alpha_small == 'e')
+			continue;
+		else
+			putchar(alpha_small++);
+	}
+	putchar('\n');
+
+	return (0);
+}
