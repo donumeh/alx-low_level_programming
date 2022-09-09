1f2ffac581afa4fb3a17eb50841da17d6fa29df7
Donald Umeh <donumeh54@gmai.com>
2022-09-09 09:05:28 -0700

    prints two digits separated by comma

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,11 +12,11 @@ int main(void)
 	for (i = 48; i < 57; i++)
 		for (j = 49; j <= 57; j++)
 		{
-			if (i > j)
+			if (i >= j)
 				continue;
 			putchar(i);
 			putchar(j);
-			if (!(i == 56 && j == 57))
+			if (i != 56 && j != 57)
 			{
 				putchar(',');
 				putchar(' ');
