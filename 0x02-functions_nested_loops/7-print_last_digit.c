28b64ba6091b33f4c02071571e1addfe1d4f5701
Donald Umeh <donumeh54@gmail.com>
2022-09-13 20:22:02 +0100

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,9 +9,13 @@ int print_last_digit(int n)
 {
 	int r;
 
-	if (n < 0)
-		n = -(n);
-	r = n % 10;
+	if (n > 0)
+		r = n % 10;
+	else if (n < 0)
+		r = (-n) % 10;
+	if (r < 0)
+		r = -r;
+
 	_putchar(r + '0');
 	return (r);
 }
