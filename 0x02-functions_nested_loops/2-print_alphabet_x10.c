d41c81320ee8a5aa972a4de7d1f89f27d4a583b4
Donald Umeh <donumeh54@gmail.com>
2022-09-13 13:49:36 +0100

    print the alpha in 10 lines

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the alphabets 10 times
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	int i, j;
+
+	for (i = 1; i <= 10; i++)
+	{
+		for (j = 97; j <= 122; j++)
+		{
+			_putchar(j);
+		}
+	_putchar('\n');
+	}
+}
