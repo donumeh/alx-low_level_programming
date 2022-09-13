a6f2988758b1e33f2a41aa0c69c4df94aae66317
Donald Umeh <donumeh54@gmail.com>
2022-09-14 00:31:09 +0100

    sumed up the first 34 fibonacci even numbers

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - prints fibonacci and sums teh even numbers
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	unsigned long int i, a, b, temp, sum = 0;
+
+	for (i = 0; i < 34; i++);
+	{
+		if ((a % 2) == 0)
+			sum += a;
+		temp = a;
+		a += b;
+		b = temp;
+	}
+	printf("%lu\n", sum);
+
+	return (0);
+}
