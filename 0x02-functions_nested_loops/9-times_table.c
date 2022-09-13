3bc1d18062a164d09e39a2f07a1c1b39cf13b138
Donald Umeh <donumeh54@gmail.com>
2022-09-13 23:18:22 +0100

    multiplication table 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,25 +14,22 @@ void times_table(void)
 		for (j = 0; j < 10; j++)
 		{
 			mul = i*j;
-			if (mul <= 9)
-			{
+			if (j == 0)
 				_putchar(mul + '0');
-				if (j != 9)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-				}
+
+			if (mul <= 9 && j != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(mul + '0');
 			}
 			else if (mul >= 10)
 			{
+				_putchar(',');
+				_putchar(' ');
 				_putchar((mul / 10) + '0');
 				_putchar((mul % 10) + '0');
-				if (j != 9)
-				{
-					_putchar(',');
-					_putchar(' ');
-				}
 			}
 		}
 		_putchar('\n');
