297efc6d935ad217fa3cfb6d1e035c4e74cf4e48
Donald Umeh <donumeh54@gmail.com>
2022-09-15 14:24:00 +0100

    print square

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_diagonal - print diagonal
+ * @n: accepts an integer
+ * Return: void
+ */
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < (i + j); j++)
+			{
+				
+			}
+		}
+	}
+}
