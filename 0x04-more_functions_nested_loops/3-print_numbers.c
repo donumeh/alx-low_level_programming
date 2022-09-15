daa885e5c97d1bae6e297ef8e64dc0a5e02b8eeb
Donald Umeh <donumeh54@gmail.com>
2022-09-15 13:07:59 +0100

    prints the numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the integer numbers
+ * Return: void
+ */
+void print_numbers(void)
+{
+	char c = '0';
+
+	while (c <= '9')
+		_putchar(c++);
+	_putchar('\n');
+}
