d560ac0a0900967170786232ff73d7b654874539
Donald Umeh <donumeh54@gmail.com>
2022-09-19 23:03:41 +0100

    print array (int)

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_array - prints the array (int)
+ * @a: array address
+ * @n: the size of array
+ * Return: void.
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i != (n - 1))
+			printf("%d, ", a[i]);
+		else
+			printf("%d\n", a[i]);
+	}
+}
