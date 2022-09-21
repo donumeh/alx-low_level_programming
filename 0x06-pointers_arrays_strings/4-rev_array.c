50a2eae17708b8a6f9efc12f4bce87d72d761295
Donald Umeh <donumeh54@gmail.com>
2022-09-21 13:59:39 +0100

    rev integer array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverses an array of int
+ * @n: sizeof array
+ * Return: void
+ */
+void reverse_array(int *a, int n)
+{
+	int i, j, store;
+
+
+	j = n / 2;
+	for (i = 0; i < j; i++)
+	{
+		store = a[i];
+		a[i] = a[n - i - 1];
+		a[n - i - 1] = store;
+	}
+}
