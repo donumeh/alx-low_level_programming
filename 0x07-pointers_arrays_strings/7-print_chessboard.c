b7ae41bffde22591b237f23050ac092fa8d60e82
Donald Umeh <donumeh54@gmail.com>
2022-09-27 15:30:30 +0100

    print chess board

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_chessboard - print the chess board
+ * @a: a pointer array of pointers
+ * Return: void
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			_putchar(*a[i]);
+			if (j == 7)
+			{
+				_putchar('\n');
+			}
+		}
+	}
+}
