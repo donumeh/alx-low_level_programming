d892ea22d5f4b9f9d2623e080b936653766a225f
Donald Umeh <donumeh54@gmail.com>
2022-09-27 15:34:04 +0100

    print chess board

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -13,11 +13,8 @@ void print_chessboard(char (*a)[8])
 	{
 		for (j = 0; j < 8; j++)
 		{
-			_putchar(*a[i]);
-			if (j == 7)
-			{
-				_putchar('\n');
-			}
+			_putchar(a[i][j]);
 		}
+		_putchar('\n');
 	}
 }
