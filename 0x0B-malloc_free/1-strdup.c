9cc5a26d6810ff66c45dabcc6acf02a905e0c46c
Donald Umeh <donumeh54@gmail.com>
2022-10-03 13:00:56 +0100

    suplicates a string using malloc

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strdup - copies a string into a pointer using malloc
+ * in order to have read and write access
+ * @str: the string
+ * Return: char pointer or NULL if malloc fails
+ */
+char *_strdup(char *str)
+{
+	int i, size = 0;
+	char *array;
+
+	if (str == NULL)
+		return (NULL);
+	while (str[size] != '\0')
+		size++;
+	array = malloc(sizeof(char) * size);
+	if (array == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+	{
+		array[i] = str[i];
+	}
+
+	return (array);
+}
