f4e3a0810a5df7bf23c3b6c01a939c40a4c9e19f
Donald Umeh <donumeh54@gmail.com>
2022-10-03 13:24:09 +0100

    concatenates two strings and initalize it to a pointer using malloc

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,16 +12,16 @@ char *str_concat(char *s1, char *s2)
 	char *array;
 
 	if (s1 == NULL)
-		s1 = '\0';
+		s1 = "";
 	else if (s2 == NULL)
-		s2 = '\0';
+		s2 = "";
 
 	while (s1[size1] != '\0')
 		size1++;
 	while (s2[size2] != '\0')
 		size2++;
 
-	array = malloc(sizeof(char) * (size1 + size2));
+	array = malloc((sizeof(char) * (size1 + size2)) + 1);
 	if (array == NULL)
 		return (NULL);
 
