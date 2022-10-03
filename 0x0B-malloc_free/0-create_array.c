a13b4225daffd03de430386651e4a05f090b101b
Donald Umeh <donumeh54@gmail.com>
2022-10-03 12:38:38 +0100

    copied into buffer mem after dynamic alloc

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * create_array - creates an array and prints a char into the buffer
+ * @size: the size of array
+ * @c: the char
+ * Return: NULL if it fails; char pointer if it works
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *array;
+	unsigned int i;
+
+	i = 0;
+	if (size <= 0)
+		return (NULL);
+
+	array = malloc((sizeof(char) * size) + 1);
+	if (array == NULL)
+	{
+		return (NULL);
+	}
+
+	for (; i < size; i++)
+	{
+		array[i] = c;
+	}
+	array[i] = '\0';
+
+	return (array);
+}
