e62bd626fa3657267d7f441be941ea490e9017c8
Donald Umeh <donumeh54@gmail.com>
2022-10-04 02:52:47 +0100

    allocates memory to a 2-dimensional array

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -16,17 +16,28 @@ int **alloc_grid(int width, int height)
 
 	array = malloc(sizeof(int) * height);
 	if (array == NULL)
+	{
 		return (NULL);
+	}
 
 	for (i = 0; i < height; i++)
 	{
 		array[i] = malloc(sizeof(int) * width);
+
 		if (array[i] == NULL)
+		{
+			free(array);
 			return (NULL);
+		}
 		for (j = 0; j < width; j++)
 		{
 			array[i][j] = 0;
 		}
 	}
+	for (i = 0; i < height; i++)
+	{
+		free(array[i]);
+	}
+	free(array);
 	return (array);
 }
