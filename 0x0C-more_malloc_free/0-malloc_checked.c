530a393eea4858a76b22c008c1cbc4bbdec04ac1
Donald Umeh <donumeh54@gmail.com>
2022-10-06 15:03:39 +0100

    allocate some memory

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -7,10 +7,10 @@
  */
 void *malloc_checked(unsigned int b)
 {
-	int *array;
+	void *array;
 	
-	array = malloc(sizeof(*array) * b);
+	array = malloc(sizeof(b));
 	if (array == NULL)
 		exit(98);
-	return ((void *)array);
+	return (array);
 }
--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,7 +9,7 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	unsigned int i;
-	char *array;
+	void *array;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
@@ -19,7 +19,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 		return (NULL);
 	for (i = 0; i < size; i++)
 	{
-		array[i] = 0;
+		*(array + i) = 0;
 	}
 	return (array);
 }
