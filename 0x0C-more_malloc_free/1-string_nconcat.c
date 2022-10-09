4ec1bc96efc80aec607d0ed4c61888fd0fff310a
Donald Umeh <donumeh54@gmail.com>
2022-10-09 11:18:37 +0100

    string concatenate

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -9,35 +9,34 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i, j = 0, len1, len2;
+	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 	char *array;
 
-	if (*s1 == '\0')
-	{
-		s1 = "";
-	}
-	if (*s2 == '\0')
-	{
-		s2 = "";
-	}
+	while (s1 && s1[len1])
+		len1++;
+	while (s2 && s2[len2])
+		len2++;
 
-	len1 = strlen(s1);
-	len2 = strlen(s2);
+	if (n < len2)
+		array = malloc(sizeof(char) * (len1 + n + 1));
+	else
+		array = malloc(sizeof(char) * (len1 + len2 + 1));
 
-	if (n >= len2)
-		n = len2;
-
-	array = malloc(sizeof(s1) * n);
-	if (array == NULL)
+	if (!array)
 		return (NULL);
 
-	for (i = 0; i < len1; i++)
+	while (i < len1)
+	{
 		array[i] = s1[i];
+		i++;
+	}
 
-	for (j = 0; j < n; j++)
-		array[i++] = s2[j];
-	
+	while (n < len2 && i < (len1 + n))
+		array[i++] = s2[j++];
+	while (n >= len2 && i < (len1 + len2 + 1))
+		array[i++] = s2[j++];
+
 	array[i] = '\0';
-	return (array);
 
+	return (array);
 }
