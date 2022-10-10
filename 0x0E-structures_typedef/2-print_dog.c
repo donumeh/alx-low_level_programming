0cb58066cfc931b16036e653efa7421c6008ba94
Donald Umeh <donumeh54@gmail.com>
2022-10-10 12:59:08 +0100

    prints elements of a struct

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,22 +10,10 @@ void print_dog(struct dog *d)
 {
 	if (d == NULL)
 		return;
-
-	printf("Name: ");
 	if (d->name == NULL)
-		printf("(nil)\n");
-	else
-		printf("%s\n", d->name);
-
-	printf("Age: ");
-	if (!d->age)
-		printf("(nil)\n");
-	else
-		printf("%.6f\n", d->age);
-
-	printf("Owner: ");
+		d->name = "(nil)";
 	if (d->owner == NULL)
-		printf("(nil)\n");
-	else
-		printf("%s\n", d->owner);
+		d->owner = "(nil)";
+
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
