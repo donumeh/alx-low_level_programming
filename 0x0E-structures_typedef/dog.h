f9ae7c953d8bd1013100c087866e29eb71872fbb
Donald Umeh <donumeh54@gmail.com>
2022-10-10 13:04:06 +0100

    defined a new type for the type struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -20,6 +20,11 @@ struct dog
 	float age;
 };
 
+/**
+ * dog_t - a new type of the struct dog
+ */
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
