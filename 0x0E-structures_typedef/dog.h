c0fa33bd2dec12945484754f0743189c38702328
Donald Umeh <donumeh54@gmail.com>
2022-10-10 12:21:22 +0100

    defined a new data type

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,22 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+/**
+ * struct dog - details about a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: This struct holds elements and attributes about
+ * a dog
+ */
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+
+#endif /* MAIN_H */
