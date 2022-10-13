1e870295e7271261f774663287874e67eaa2f700
Donald Umeh <donumeh54@gmail.com>
2022-10-13 23:11:01 +0100

    prints numbers

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,29 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - prints the numbers passed as param
+ * @separator: char pointer
+ * @n: the number of param in variadic
+ *
+ * Return: void
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	if (separator)
+	{
+		va_list ap;
+		unsigned int i;
+
+		va_start(ap, n);
+
+		for (i = 0; i < n; i++)
+		{
+			if (i == (n - 1))
+			{
+				printf("%d\n", va_arg(ap, int));
+			}
+			else
+				printf("%d%s", va_arg(ap, int), separator);
+		}
+	}
+}
