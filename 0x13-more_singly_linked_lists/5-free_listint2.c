54b9375aff2c280259161d05d0c03c8c6814d88e
Donald <donumeh54@gmail.com>
2022-10-25 21:37:27 +0100

    freed linked list

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,15 +8,15 @@
  */
 void free_listint2(listint_t **head)
 {
+	listint_t *current;
 	listint_t *temp;
-	listint_t *keep;
 
-	keep = *head;
-	while (keep)
+	current = *head;
+	while (current)
 	{
-		temp = keep->next;
-		free(keep);
-		keep = temp;
+		temp = current->next;
+		free(current);
+		current = temp;
 	}
 	*head = NULL;
 }
