48ef103e1cc541dc94ad01afffbce876644391d2
Donald <donumeh54@gmail.com>
2022-12-22 02:45:59 +0100

    prints a string using a specifier

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,10 +1,12 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef VAR_FUNC
+#define VAR_FUNC
 
 #include <stdio.h>
 #include <stdarg.h>
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 
-#endif /* MAIN_H */
+#endif /* VAR_FUNC */
