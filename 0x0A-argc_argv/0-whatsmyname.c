4922023ddc0cd23781edb680ed761790b658a29c
Donald <user@example.com>
2023-06-09 18:37:42 +0100

    Print the name of the program

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of the program
+ * @argc: the number of argument passed
+ * @argv: the array to CLI
+ *
+ * Return: Always 0
+ */
+
+int main( __attribute__((unused)) int argc, char **argv)
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
