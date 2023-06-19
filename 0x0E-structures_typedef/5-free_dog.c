0cd163c409302b522f9d3599fcf7cca6c0a566c8
Donald <user@example.com>
2023-06-19 10:46:22 +0100

    Free memory storage created to save dog details

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+
+/**
+ * free_dog - frees the memory storage of dog details
+ * @d: the structure or object of dog
+ *
+ * Return: void
+ */
+
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
