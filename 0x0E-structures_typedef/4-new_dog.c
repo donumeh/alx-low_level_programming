193261e32232e90ae8bbbc1494f41ccd81469507
Donald <user@example.com>
2023-06-20 09:00:25 +0100

    create a new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -14,41 +14,24 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *my_dog = NULL;
 
-	if (!name)
-	{
-		name = malloc(sizeof("(nil)"));
-		if (name == NULL)
-			return (NULL);
-		_strcpy(name, "(nil)");
-	}
-	if (!owner)
-	{
-		owner = malloc(sizeof("(nil)"));
-		if (owner == NULL)
-			return (NULL);
-		_strcpy(owner, "(nil)");
-	}
-
 	my_dog = malloc(sizeof(dog_t));
 	if (my_dog == NULL)
 		return (NULL);
 
-	my_dog->name = malloc(sizeof(name));
+	my_dog->name = strdup(name);
 	if (my_dog->name == NULL)
 	{
 		free(my_dog);
 		return (NULL);
 	}
-	_strcpy(my_dog->name, name);
 
-	my_dog->owner = malloc(sizeof(owner));
+	my_dog->owner = strdup(owner);
 	if (my_dog->owner == NULL)
 	{
 		free(my_dog->name);
 		free(my_dog);
 		return (NULL);
 	}
-	_strcpy(my_dog->owner, owner);
 	my_dog->age = age;
 
 	return (my_dog);
