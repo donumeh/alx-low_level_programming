3150088da54943bf65d162a4dd9c8aa1be81b896
Donald <user@example.com>
2023-06-21 19:51:05 +0100

    A calculator

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,43 @@
+#include "3-calc.h"
+
+/**
+ * main - calculate two operands
+ * @argc: the number of args passed
+ * @argv: the strings or args passed
+ *
+ * Return: 0
+ */
+
+int main(int argc, char **argv)
+{
+	int (*ptr)(int, int);
+	int result, h, u;
+
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	h = atoi(argv[1]);
+	u = atoi(argv[3]);
+
+	ptr = get_op_func(argv[2]);
+	if (ptr == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	
+	if (((strcmp(argv[2], "/") == 0) || (strcmp(argv[2], "%") == 0))
+				&& h == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	result = ptr(h, u);
+	printf("%d\n", result);
+
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,66 @@
+#include "3-calc.h"
+
+/**
+ * op_add - add two int
+ * @a: the first int
+ * @b: the second int
+ *
+ * Return: int
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - sub two int
+ * @a: the first int
+ * @b: the second int
+ *
+ * Return: int
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - mul two ints
+ * @a: thr first int
+ * @b: the second int
+ *
+ * Return: mul two int
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - div two int
+ * @a: the first int
+ * @b: the second int
+ *
+ * Return: int
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - mod two int
+ * @a: first int
+ * @b: second int
+ *
+ * Return: int
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
