8fad8cd5a630184f4add85e8dc7e93fb3b2610f2
Donald <user@example.com>
2023-06-21 10:24:58 +0100

    Print a name using a function pointer as arg

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints name
+ * @name: the name to print
+ * @f: the function pointer to use in printing
+ *
+ * Return: void
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
