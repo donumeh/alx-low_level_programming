2e2fb1a537391ae85c87307b821765b38b1f531e
Donald <user@example.com>
2023-06-22 22:43:54 +0100

    Prints strings

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -16,6 +16,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_list copy;
 
 	va_start(ap, n);
+	va_start(copy, format);
 	va_copy(copy, ap);
 	for (i = 0; i < n; i++)
 	{
