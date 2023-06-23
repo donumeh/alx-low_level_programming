25e68d561cc5008aeb03fc58556c9fdf17423b43
Donald <user@example.com>
2023-06-23 13:39:12 +0100

    Checked if NULL is passed in string

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -14,27 +14,35 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	unsigned int i, len;
 	va_list ap;
 	va_list copy;
+	va_list cp2;
 
 	va_start(ap, n);
 	va_copy(copy, ap);
 	for (i = 0; i < n; i++)
 	{
-		len = strlen(va_arg(ap, char *));
-		string = malloc(sizeof(char) * (len + 1));
-		if (string == NULL)
-			exit(EXIT_FAILURE);
-
-		strcpy(string, va_arg(copy, char *));
-		if (string == NULL)
-			strcpy(string, "(nil)");
-		printf("%s", string);
-
+		va_copy(cp2, ap);
+		if (va_arg(cp2, char *) == NULL)
+		{
+			printf("(nil)");
+		}
+		else
+		{
+			len = strlen(va_arg(ap, char *));
+			string = malloc(sizeof(char) * (len + 1));
+			if (string == NULL)
+				exit(EXIT_FAILURE);
+			strcpy(string, va_arg(copy, char *));
+			if (string == NULL)
+				strcpy(string, "(nil)");
+			printf("%s", string);
+			free(string);
+		}
 		if ((separator != NULL) && i != (n - 1))
 			printf("%s", separator);
-		free(string);
 	}
 	printf("\n");
 
+	va_end(cp2);
 	va_end(copy);
 	va_end(ap);
 }
