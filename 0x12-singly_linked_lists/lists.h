8e97ddb037a67b64cce5f152e6335bca31fde3f6
Donald <user@example.com>
2023-07-04 09:59:41 +0100

    Prints the nodes in a linked list

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -7,11 +7,10 @@
 
 
 /**
- *
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
  * @len: length of the string
- * @nest: points to the next node
+ * @next: points to the next node
  *
  * Description: singly linked list node structure
  */
