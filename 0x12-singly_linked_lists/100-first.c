09e8c054b7bc2369e6448ccf7e1f2a5f8b4a6fcf
Donald <user@example.com>
2023-07-04 11:05:23 +0100

    Prints a function before main. Can be used to perform a clean up before executing the code.

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 
-void first() __attribute__ ((constructor));
+void first(void) __attribute__ ((constructor));
 /**
  * first - prints an algorithm first
  *
@@ -9,5 +9,6 @@ void first() __attribute__ ((constructor));
 
 void first(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n
+			I bore my house upon my back!\n");
 }
