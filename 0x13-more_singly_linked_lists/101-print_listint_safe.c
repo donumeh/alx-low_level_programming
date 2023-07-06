8ab07f86d9d8ce9967f389d5d8abbbc65880626e
Donald <user@example.com>
2023-07-06 19:47:46 +0100

    Prints a linked list

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -11,12 +11,12 @@ size_t print_listint_safe(const listint_t *head)
 {
 	const listint_t *current = head;
 	size_t node_count = 0;
-	
+
 	while (current != NULL)
 	{
 		printf("[%p] %d\n", (void *)current, current->n);
 		node_count++;
-		
+
 		if (current <= current->next)
 		{
 			printf("-> [%p] %d\n", (void *)current->next,
@@ -26,5 +26,5 @@ size_t print_listint_safe(const listint_t *head)
 		current = current->next;
 	}
 
-	return node_count;
+	return (node_count);
 }
