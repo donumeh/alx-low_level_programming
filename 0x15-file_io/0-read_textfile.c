69f59c5d370da44e910487cba13ffa0cbb295e84
Donald <user@example.com>
2023-07-11 18:43:00 +0100

    Reads a text file and prints it

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * read_textfile - read a text file
+ * @filename: name of file to read
+ * @letters: the number of letters to read from file
+ *
+ * Return: number of bytes printed
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	char *str;
+	ssize_t n;
+
+	if (!filename)
+		return (0);
+	/* read the file with open */
+	fd = open(filename, O_RDONLY);
+
+       /* checks if error is encountered during opening */
+	if (fd < 0)
+		return (0);
+
+	/* allocate memory to store the characters read for printing */
+	str = malloc(sizeof(char) * letters);
+	if (str == NULL)
+		return (0);
+
+	/* read the file */
+	n = read(fd, str, letters);
+	if (n < 0)
+		return (0);
+
+	/* print the letters */
+	dprintf(2, "%s", str);
+
+	close(fd);
+	free(str);
+	return (n);
+}
---
+++ 0x15-file_io/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* header files */
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+/* prototypes */
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif /* MAIN_H */
