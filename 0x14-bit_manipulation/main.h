0179cac29ec00b4dc484b8e884653a8aa4d599ad
Donald <user@example.com>
2023-08-03 13:45:30 +0100

    converts a binary to int

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+unsigned int binary_to_uint(const char *b);
+int _strlen(const char *);
+unsigned int char_to_int(char, int *);
+unsigned int _pow(int, int);
+
+#endif /* MAIN_H */
