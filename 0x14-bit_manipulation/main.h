a03ff4112f2309b490da9a506895f4d572d26b4b
Donald <user@example.com>
2023-08-03 14:09:52 +0100

    Converts an integer to binary

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -2,10 +2,13 @@
 #define MAIN_H
 
 #include <stdio.h>
+#include <unistd.h>
 
 unsigned int binary_to_uint(const char *b);
 int _strlen(const char *);
 unsigned int char_to_int(char, int *);
 unsigned int _pow(int, int);
+void print_binary(unsigned long int n);
+int _putchar(char);
 
 #endif /* MAIN_H */
