4a5365c8817e6e3bdd0876a3b9a74890cac06ff1
Donald <user@example.com>
2023-08-03 15:55:57 +0100

    Get bit from a value at an integer

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,12 +10,12 @@
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-/**	unsigned long int mask;
+/* 	unsigned long int mask; */
 
 	if (index >= sizeof(unsigned long int) * 8)
 		return (-1);
 
-	mask = 1UL << index;
+/*	mask = 1UL << index;
 */
 	n >>= index;
 	return (n & 1 ? 1 : 0);
