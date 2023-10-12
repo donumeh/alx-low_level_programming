a17f170f8d6b84dfa871e23e63c595aa77ba5139
Donald <user@example.com>
2023-10-12 13:03:01 +0100

    add a node at the beginning of a linked list

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -21,6 +21,8 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 
 	new_node->n = n;
 	new_node->next = *head;
+	if (*head != NULL)
+		(*head)->prev = new_node;
 	new_node->prev = NULL;
 
 	*head = new_node;
