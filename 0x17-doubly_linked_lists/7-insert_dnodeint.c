f5a70c941e43a39f992c85c68793a89e67279c57
Donald <user@example.com>
2023-10-12 15:42:17 +0100

    function that inserts a new node at an index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -17,7 +17,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h,
 
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
-		exit(EXIT_FAILURE);
+		return (NULL);
 	new_node->n = n;
 	tmp = *h;
 	if (idx == 0)
