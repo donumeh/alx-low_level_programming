b71282fae50548a2953ad440e0a9ea215f076645
Donald <user@example.com>
2023-10-12 16:18:30 +0100

    a function that adds a node at the end of a doubly linked list

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -15,7 +15,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	tmp = *head;
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
-		exit(EXIT_FAILURE);
+		return (NULL);
 
 	new_node->n = n;
 
