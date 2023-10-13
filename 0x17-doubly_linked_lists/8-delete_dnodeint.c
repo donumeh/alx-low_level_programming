51f191f4ecbad1ab1ad6594c56392abd490cc1ca
Donald <user@example.com>
2023-10-13 16:04:34 +0100

    delete a node at an index

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -14,6 +14,12 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	unsigned int i = 0;
 
 	current = *head;
+	if (index == 0)
+	{
+		*head = (*head)->next;
+		free(current);
+		return (1);
+	}
 	while (current != NULL)
 	{
 		if (i == index)
