e77626f9831c8db621e790a743c6842605033b14
Donald <user@example.com>
2023-11-04 01:40:59 +0100

    print nested dictionary or hash table

---
+++ 0x1A-hash_tables/5-hash_table_print_nested.c
@@ -0,0 +1,51 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - prints the keys and values
+ * @ht: the hash table
+ *
+ * Return: void
+ */
+
+void hash_table_print_nested(const hash_table_t *ht)
+{
+	unsigned long int i;
+	int j = 0, h = 0;
+	hash_node_t *h_nd;
+
+	if (ht == NULL)
+		return;
+
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i] != NULL)
+		{
+			h_nd = ht->array[i], j++;
+			if (h_nd->next != NULL)
+			{
+				if (j > 1)
+					printf(", ");
+				printf("{"), h = 0;
+				while (h_nd != NULL)
+				{
+					h++;
+					if (h == 1)
+						printf("'%s': '%s'", h_nd->key, h_nd->value);
+					else
+						printf(", '%s': '%s'", h_nd->key, h_nd->value);
+					h_nd = h_nd->next;
+				}
+				printf("}");
+			}
+			else
+			{
+				if (j == 1)
+					printf("'%s': '%s'", h_nd->key, h_nd->value);
+				else
+					printf(", '%s': '%s'", h_nd->key, h_nd->value);
+			}
+		}
+	}
+	printf("}\n");
+}
